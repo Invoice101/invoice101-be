2637bbae158bdcf92b5702b88050a2d9761a695f
Karthik RP <karthik.rp@hotmail.com>
2020-04-18 15:20:04 +0530

    Modified local secret key.

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,10 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY",
-    default="wx6h6cW6IB2j21Nd1jBHXdgzlIyUTaEYZL2T7oOtgToz2EC1SHKaVMQhLsZoRBBO",
-)
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
