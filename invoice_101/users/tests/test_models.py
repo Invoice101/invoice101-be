d6314328fff276ec0f4bd6553ba5301b72c9c3c4
Karthik RP <karthik.rp@hotmail.com>
2020-04-19 10:53:37 +0530

    mypy checks.

--- invoice_101/users/tests/test_models.py
+++ invoice_101/users/tests/test_models.py
@@ -1,9 +1,3 @@
 import pytest
 
-from invoice_101.users.models import User
-
 pytestmark = pytest.mark.django_db
-
-
-def test_user_get_absolute_url(user: User):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
