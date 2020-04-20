01da5242df753a3363e4d35228f2c1e516c22f6a
Karthik RP <karthik.rp@hotmail.com>
2020-04-20 17:54:03 +0530

    Favicon reddirect.

--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
                   path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-                  path(
-                      "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-                  ),
+                  path('favicon.ico', RedirectView.as_view(url='/static/images/favicons/favicon.ico')),
                   path(settings.ADMIN_URL, admin.site.urls),
                   path("accounts/", include("allauth.urls")),
                   # Your stuff: custom urls includes go here
