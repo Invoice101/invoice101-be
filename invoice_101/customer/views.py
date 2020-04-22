6967bb14d96c1e5bb2e2fc756e1178117f43f7e7
Karthik RP <karthik.rp@hotmail.com>
2020-04-22 16:42:29 +0530

    WIP Permissions.

--- invoice_101/customer/views.py
+++ invoice_101/customer/views.py
@@ -4,12 +4,14 @@ from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, CreateMode
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Customer
+from .permissions import CustomerPermission
 from .serializers import CustomerSerializer
 from ..utils.common_utils import StandardResultsSetPagination
 
 
 class CustomerViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, CreateModelMixin, UpdateModelMixin):
     serializer_class = CustomerSerializer
+    permission_classes = [CustomerPermission]
     queryset = Customer.objects.all()
     pagination_class = StandardResultsSetPagination
     filter_backends = (SearchFilter,)
@@ -18,4 +20,6 @@ class CustomerViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, Create
     def get_queryset(self):
         sort = self.request.query_params.get('sort', 'name')
         queryset = self.get_queryset().order_by(sort)
+
+        queryset = queryset.filter(owner=self.request.user)
         return queryset
